60f8a6c63a1509c7309f39a704fb8b88edc5dc7a
silverrangercactus <silverrangercactus@gmail.com>
2022-03-08 21:16:10 +0500

    commit for rebase

--- myyy/myyy/ViewController.swift
+++ myyy/myyy/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("1")
-        
+        print("for rebase")
     }
     
     
