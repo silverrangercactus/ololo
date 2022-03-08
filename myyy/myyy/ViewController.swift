ffe73c9fce7200c6142c9f3a7b69cbc39e3fb72a
silverrangercactus <silverrangercactus@gmail.com>
2022-03-08 20:59:13 +0500

    commit1

---
+++ myyy/myyy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myyy
+//
+//  Created by Fedor Boriskin on 08.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("1")
+        
+    }
+    
+    
+}
+
